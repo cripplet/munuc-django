243c9796646b38f2a1a3bd363f586b351d7b1cc6
Minke Zhang <minke.zhang@gmail.com>
2016-10-04 22:42:47 -0700

    Remove extra staffers serializers

--- atlas/staffers/serializers.py
+++ atlas/staffers/serializers.py
@@ -1,43 +1,6 @@
 from rest_framework import serializers
 
 from atlas.models import Committee
-from atlas.models import CommitteeStaffer
-from atlas.models import Delegation
-from atlas.models import InternalUser
-from atlas.models import BaseUser
-from atlas.models import USGGroup
-
-
-class InternalUserSerializer(serializers.HyperlinkedModelSerializer):
-  url = serializers.HyperlinkedIdentityField(view_name='staffers:internaluser-detail', read_only=True)
-  first_name = serializers.CharField(source='user.first_name', read_only=True)
-  last_name = serializers.CharField(source='user.last_name', read_only=True)
-  usg_group = serializers.HyperlinkedRelatedField(view_name='staffers:usggroup-detail', read_only=True)
-
-  class Meta:
-    model = InternalUser
-    read_only = True
-    exclude = ('user',)
-
-
-class CommitteeStafferSerializer(serializers.HyperlinkedModelSerializer):
-  url = serializers.HyperlinkedIdentityField(view_name='staffers:committeestaffer-detail', read_only=True)
-  first_name = serializers.CharField(source='user.first_name', read_only=True)
-  last_name = serializers.CharField(source='user.last_name', read_only=True)
-  committee = serializers.HyperlinkedRelatedField(view_name='staffers:committee-detail', read_only=True)
-
-  class Meta:
-    model = CommitteeStaffer
-    read_only = True
-    exclude = ('user', 'email',)
-
-
-class DelegationSerializer(serializers.HyperlinkedModelSerializer):
-  url = serializers.HyperlinkedIdentityField(view_name='staffers:delegation-detail', read_only=True)
-  committees = serializers.HyperlinkedRelatedField(many=True, view_name='staffers:committee-detail', read_only=True)
-  class Meta:
-    model = Delegation
-    read_only = True
 
 
 class CommitteeSerializer(serializers.HyperlinkedModelSerializer):
@@ -50,13 +13,3 @@ class CommitteeSerializer(serializers.HyperlinkedModelSerializer):
     model = Committee
     read_only = True
     exclude = ('delegations', 'usg_group')
-
-
-class USGGroupSerializer(serializers.HyperlinkedModelSerializer):
-  url = serializers.HyperlinkedIdentityField(view_name='staffers:usggroup-detail', read_only=True)
-  committees = serializers.HyperlinkedRelatedField(many=True, view_name='staffers:committee-detail', read_only=True)
-  usg = serializers.HyperlinkedRelatedField(view_name='staffers:internaluser-detail', read_only=True)
-
-  class Meta:
-    model = USGGroup
-    read_only = True
